cea2eec35eac75405d61651b719960b183957e3d
DanPark13 <dpark0703@gmail.com>
2023-09-25 16:49:49 -0400

    Add Input + Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [answer, setAnswer] = useState<string>('');
+  
+  const handleSearch = async (query: string) => {
+    try {
+      const response = await axios.post<{ answer: string }>("https://localhost:5173/search", {query})
+      setAnswer(response.data.answer);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TutorAI</h1>
+      <SearchBar onSearch={handleSearch}/>
+      <div>{answer}</div>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+interface SearchBarProps {
+    onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [query, setQuery] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSearch(query);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Input Question Here..."
+            />
+            <button type="submit">Search</button>
+        </form>
+    )
+}
+
+export default SearchBar;
